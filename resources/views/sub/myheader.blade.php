b828af26902c2bf951b3320ecf8ac929d66aa748
foxdeveloper4030 <arsalanjalil75@gmail.com>
2021-07-23 03:10:26 +0430

    search index css

--- resources/views/sub/myheader.blade.php
+++ resources/views/sub/myheader.blade.php
@@ -23,8 +23,8 @@
                                 onclick="window.location.href='{{url('')}}/search/'+document.getElementById('gsearchsimple').value">
                             <img src="{{asset('public/assets/img/search.png')}}" alt=""></button>
                         <div class="overlay-search-box">
-                            <div style="background-color: red">
-                                4444
+                            <div style="background-color: white;height: 200px;border: 1px solid #f44336;border-radius: 20px;padding: 10px;" >
+                                <span class="badge" style="border: 1px solid #f44336;background-color: grey">44</span>
                             </div>
                         </div>
                     </form>
