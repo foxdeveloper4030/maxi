fa49d8737488e2d1d0f20ec49a85268b33dcbe4e
foxdeveloper4030 <arsalanjalil75@gmail.com>
2021-07-23 03:40:28 +0430

    search index html

--- resources/views/sub/myheader.blade.php
+++ resources/views/sub/myheader.blade.php
@@ -35,9 +35,11 @@
                                 </div>
                                 <hr>
                                 <div style="margin: 5px">
-                                    <span class="badge" style="border: 1px solid #f44336;background-color: #dcdada">44</span>
-                                    <span class="badge" style="border: 1px solid #f44336;background-color: #dcdada">44</span>
-                                    <span class="badge" style="border: 1px solid #f44336;background-color: #dcdada">44</span>
+                                    @foreach(\App\Product::query()->orderBy('view')->limit(10)->get() as $product)
+                                    <span class="badge" style="border: 1px solid #f44336;background-color: #dcdada">
+                                        {{$product->name}}
+                                    </span>
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
