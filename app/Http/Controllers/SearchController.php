85e2f9dd9b53c85eb55b425b811b9ce5931cb0a7
foxdeveloper4030 <arsalanjalil75@gmail.com>
2021-07-23 04:34:38 +0430

    search index html

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Product;
 use Illuminate\Http\Request;
 
@@ -10,8 +11,13 @@ class SearchController extends Controller
     public function search($name){
         $products=Product::query()->orWhere('name', 'like', '%' . $name . '%')->Where('price_main','>',1000);
        $count=count(Product::query()->orWhere('name', 'like', '%' . $name . '%')->Where('price_main','>',1000)->get());
+       $categories=Category::query()->orWhere('name', 'like', '%' . $name . '%');
+
       if (isset($_GET['result'])){
           $products=$products->where('name','like','%'.$_GET['result'].'%');
+          foreach ($categories as $category){
+              $products->addBinding($category->products);
+          }
           $count=count($products->get());
 
       }
